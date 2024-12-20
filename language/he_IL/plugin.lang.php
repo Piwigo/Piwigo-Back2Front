8ed607eb6e47d4331005c7593803afad7f5454b4
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 18:07:23 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,34 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['...of the picture n°'] = '...של התמונה מספר n° 
+';
+$lang['Backside and frontside can\'t be the same picture'] = 'הצד האחורי והצד הקדמי לא יכולים להיות אותה תמונה';
+$lang['Backside management'] = 'ניהול הצד האחורי';
+$lang['Bottom'] = 'תחתית';
+$lang['Click'] = 'לחץ';
+$lang['Display a mark on thumbnails list'] = 'הצג סימן ברשימת התמונות הממוזערות';
+$lang['Fade'] = 'עמעם';
+$lang['Hide backside from albums'] = 'הסתר את הצד האחורי מהאלבומים';
+$lang['Link name'] = 'שם לחצן';
+$lang['Link position'] = 'מיקום הלחצן';
+$lang['Modify backside information'] = 'שנה את מידע הצד האחורי';
+$lang['Mouseover'] = 'על פי העברת העכבר';
+$lang['See back'] = 'ראה את הצד האחורי';
+$lang['See front'] = 'ראה את הצד הקדמי';
+$lang['Seperate the two labels with the | symbol. Leave blank to use default translation.'] = 'הפרד את שני התוויות בסימן |. השאר ריק כדי להשתמש בתרגום ברירת המחדל.';
+$lang['Switch mode'] = 'מעבר למצב';
+$lang['The picture n°%d has already a backside : %s'] = 'התמונה מספר %d כבר יש לה צד אחורי: %s';
+$lang['The picture n°%s is already a backside'] = 'התמונה מספר %s היא כבר צד אחורי';
+$lang['This picture has a backside :'] = 'לתמונה זו יש צד אחורי:';
+$lang['This picture is a backside...'] = 'תמונה זו היא צד אחורי...';
+$lang['This picture is no longer a backside'] = 'תמונה זו כבר אינה צד אחורי';
+$lang['This picture is now the backside of the picture n°%s'] = 'תמונה זו כעת היא הצד האחורי של התמונה מספר %s';
+$lang['Toolbar'] = 'סרגל הכלים';
+$lang['Top'] = 'עליון';
+$lang['Transition'] = 'מעבר';
+$lang['Unknown id %d for frontside picture'] = 'מזהה לא ידוע %d עבור תמונת צד קדמי';
